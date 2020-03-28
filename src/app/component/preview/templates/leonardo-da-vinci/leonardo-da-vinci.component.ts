04b9012e5af4728c38d9ccf397fae07b9f379741
i.lojan@yahoo.com <lojan1994@>
2020-03-28 20:18:38 +0530

    tm test

--- src/app/component/preview/templates/leonardo-da-vinci/leonardo-da-vinci.component.ts
+++ src/app/component/preview/templates/leonardo-da-vinci/leonardo-da-vinci.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
-import { ResumeService } from 'src/app/resume.service';
-import { UtilityService } from 'src/app/utility.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-leonardo-da-vinci',
@@ -9,30 +7,9 @@ import { UtilityService } from 'src/app/utility.service';
 })
 export class LeonardoDaVinciComponent implements OnInit {
 
-
-  @ViewChild("leonardo", { static: false }) einsteinPage: ElementRef;
-  einsteinPage2 = true;
-  templateData: any;
-  page2Class = "secondPageEes";
-
-  constructor(
-    public resumeService: ResumeService,
-    private renderer2: Renderer2, private el: ElementRef, private urilityService: UtilityService) {
-
-  }
-  ngAfterViewInit(): void {
-
-    setTimeout(() => {
-      if (this.urilityService.isObjectIsNotNull(this.resumeService.resumeComponents)) {
-        this.einsteinPage2 = this.urilityService.templatePageBreak(this.einsteinPage, this.page2Class);
-      }
-    }, 1000)
-
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.resumeService.loadResumeComponentsJson();
   }
-
 
 }
